7fbb27ecc119cc6e27c71d1799df28a1dd4415d8
CoolRy <that007guy@hotmail.com>
2022-06-01 19:41:47 -0400

    exercise 6e

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
-const winningCombos = [0,1,2]; [3,4,5]; [6,7,8]; [0,3,6]; [1,7,3]; [2,5,8];[2,4,6]; [0,4,8] 
+const winningCombos = [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,7,3], [2,5,8],[2,4,6], [0,4,8] 
 
 
 
@@ -11,33 +11,40 @@ let winner = null
 
 
 
+
 /*------------------------ Cached Element References ------------------------*/
 const messageEl = document.querySelector('#message')
-const squareEls = document.querySelectorAll('#sq0', '#sq1', '#sq2', '#sq3', '#sq4', '#sq5', '#sq6', '#sq7', '#sq8')
+const squareEls = document.querySelectorAll('.square')
+
+
+
 
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-board.addEventListener('click', handleClick)
-
-
+squareEls.forEach(function(square){
+  square.addEventListener('click', handleClick)
+})
 
 /*-------------------------------- Functions --------------------------------*/
 init ()
 
 function init () {
+  board = [null, null, null, null, null, null, null, null, null]
+  let turn = 1
+  let winner = null
+  render ()
 }
-// render ()
 
 
 function render () {
   board.forEach(function(square, idx){
-    if (square === -1) {
-      squareEls[idx].textContent ='O'
-    } else if (square === 1) {
-      squareEls[idx].textContent = 'X'
+    if (square === 1) {
+      squareEls[idx].textContent ='X'
+    } else if (square === -1) {
+      squareEls[idx].textContent = 'O'
     } else if (square === null) {
       squareEls[idx].textContent = null
 
@@ -51,6 +58,12 @@ function render () {
 }
 
 function handleClick(evt){
+  const sqIdx = parseInt(evt.target.id.slice(2))
+  if (winner !== null){
+    return `Game is over`
+  } else if (board === sqIdx){
+    return `square has been taken`
+  }
 
 }
 
